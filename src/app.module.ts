5b41fe0f457a806e012ffd37063d8b33db561a0e
Daniel Hockley <daniel@tforge.co.za>
2023-11-21 18:22:01 +0200

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { WordsModule } from './words/words.module';
-import { SentencesModule } from './sentences/sentences.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { MongooseModule } from '@nestjs/mongoose'
+import { WordsModule } from './words/words.module'
+import { SentencesModule } from './sentences/sentences.module'
 
 @Module({
-  imports: [
-    // MongooseModule.forRoot(
-    //   'mongodb+srv://danhockley1:06ymj6tqHJWDjCG2@sentencebuilder.fgmnxoo.mongodb.net/',
-    // ),
-    // WordsModule,
-    // SentencesModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        // MongooseModule.forRoot(
+        //     'mongodb+srv://danhockley1:06ymj6tqHJWDjCG2@sentencebuilder.fgmnxoo.mongodb.net/',
+        // ),
+        WordsModule,
+        // SentencesModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
