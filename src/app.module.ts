7965cf848438a15907091e63e2c18f3f92f695ec
Daniel Hockley <daniel@tforge.co.za>
2023-11-21 18:16:22 +0200

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { WordsModule } from './words/words.module';
+import { SentencesModule } from './sentences/sentences.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://danhockley1:06ymj6tqHJWDjCG2@sentencebuilder.fgmnxoo.mongodb.net/',
+    // ),
+    WordsModule,
+    SentencesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
