eaa9e7e2c5d1565e37d3d59b68448205230d0683
Daniel Hockley <daniel@tforge.co.za>
2023-11-21 18:42:35 +0200

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,9 @@ import { SentencesModule } from './sentences/sentences.module'
 
 @Module({
     imports: [
-        MongooseModule.forRoot(
-            'mongodb+srv://danhockley1:06ymj6tqHJWDjCG2@sentencebuilder.fgmnxoo.mongodb.net/',
-        ),
+        // MongooseModule.forRoot(
+        //     'mongodb+srv://danhockley1:06ymj6tqHJWDjCG2@sentencebuilder.fgmnxoo.mongodb.net/',
+        // ),
         WordsModule,
         // SentencesModule,
     ],
